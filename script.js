3e209a3fe1abf39e5018173811826d2731bf2d4d
Divya Dharshan S <97381430+Divyadharshan@users.noreply.github.com>
2024-01-19 16:03:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function getdays(year,month){
   return new Date(year,month,0).getDate();
 }
 function Age(){
-  let d1 = document.getElementById("d").value;
-  let m1 = document.getElementById("m").value+1;
-  let y1 = document.getElementById("y").value; 
+  let d1 = parseInt(document.getElementById("d").value);
+  let m1 = parseInt(document.getElementById("m").value)+1;
+  let y1 = parseInt(document.getElementById("y").value); 
   let show = document.querySelector('p');
   let today = new Date();
   let d2 = today.getDate();
-  let m2 = today.getMonth()+1;
+  let m2 = today.getMonth()+2;
   let y2 = today.getFullYear();
   let d3,m3,y3;
   y3=y2-y1;
@@ -31,7 +31,6 @@ function Age(){
     m3=11;
     y3--;
   }
-   console.log(y3,m3,d3);
     show.innerText=`You are ${y3} years, ${m3} months, ${d3} days old.`;
 }
 bu.addEventListener("click",Age);
